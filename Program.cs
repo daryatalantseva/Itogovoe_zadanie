8fb2dc1fbb6cdf662800c9bad6ad357c0b2b72df
daryatalantseva <d.talantseva@gmail.com>
2022-09-18 12:14:27 +0300

    Итог

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. */
 
 string[] array_str = new string[15] {"Vasya", "6987", "diplomat", "мир", "100", "мир", "15", "xo-xo", "007", "dog", "cap", "Задача", "0190283", "Summer", "ITOG"};
-string[] array2 = new string[array_str.Length];
+string[] array_itog = new string[array_str.Length];
 int count = 0;
 for (int i = 0; i < array_str.Length; i++)
     {
     if(array_str[i].Length <= 3)
         {
-        array2[count] = array_str[i];
+        array_itog[count] = array_str[i];
         count++;
         }
     }
@@ -20,3 +20,8 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+Console.WriteLine("Начальный массив");
+PrintArray(array_str);
+Console.WriteLine();
+Console.WriteLine("Итоговый массив");
+PrintArray(array_itog);
