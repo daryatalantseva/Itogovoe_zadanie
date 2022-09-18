d9e061798d3be261815a7a96c8ae482f2c24b121
daryatalantseva <d.talantseva@gmail.com>
2022-09-18 12:13:57 +0300

    Метод вывода массива

--- Program.cs
+++ Program.cs
@@ -12,3 +12,11 @@ for (int i = 0; i < array_str.Length; i++)
         count++;
         }
     }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
